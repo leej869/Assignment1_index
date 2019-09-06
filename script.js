12c0cf6c1e3013f23d24fc3e906e1803868d4d2f
Juna Lee <leej869@newschool.edu>
2019-09-06 13:55:37 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,27 @@
-console.log("test")
+ var Myvariable = 'this is a string';
+
+ var minute = 17;
+
+ var hour = 'string';
+
+ var className = 'web advanced'
+
+ var myArray = [];
+ myArray = [2,3,4];
+ myArray = 'string';
+
+
+ if (minute < 20 ) {
+ 	console.log("hello!")
+ } else if(minute = 18){
+ 	console.log("elsewhere")
+ }
+
+
+ var iteration = 0;
+ 
+ console.log(iteration)
+ 
+ iteration++;
+ 
+ console.log(iteration);
